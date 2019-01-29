0fe910f7b13dd3b88b231fe119836c48537e9107
RGrouse <besedin-r-i@yandex.ru>
2019-01-29 23:34:05 +0300

    Подчистил остатки python

--- main.js
+++ main.js
@@ -3,26 +3,17 @@ const {app, BrowserWindow} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
-let pythonService
+let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1000, height: 600})
+  mainWindow = new BrowserWindow({width: 1000, height: 600});
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('index.html');
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-
-  // Spawn python service
-  const childProcess = require('child_process');
-  pythonService = childProcess.spawn('python3.6',["../src/rpc_service.py"])
-  pythonService.stdout.removeAllListeners("data");
-  pythonService.stderr.removeAllListeners("data");
-  pythonService.stdout.pipe(process.stdout);
-  pythonService.stderr.pipe(process.stderr);
+  mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -36,7 +27,7 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -45,23 +36,12 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
 
 app.on('activate', function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow()
-  }
-})
-
-// App close handler
-app.on('before-quit', function() {
-  try {
-    process.kill(pythonService.pid, 'SIGINT');
-    console.log( 'Quitting process with SIGINT' );
-  }
-  catch(error) {
-    console.error(error);
   }
 });
--- renderer.js
+++ renderer.js
@@ -1,89 +1,21 @@
-var net = require('net');
+document.getElementById('signIn').onclick = function(){
+    doLog('Click!');
+};
 
-ip = '127.0.0.1'
-port = 4242
+document.getElementById('uploadToBlockchain').onclick = function(){
+    let filePath = document.getElementById('fileToUpload').files[0].path;
+    doLog(filePath);
+};
 
-var timer;
-timeout = 3000;
-try {
-    console.log("[INFO] connecting to " + ip + ":" + port);
-    clientSocket = new net.createConnection(port,ip)
-        .on('connect', function() {
-            clearTimeout(timer);
-            console.log("connected");
-        })
-        .on('data', function() {
-            clearTimeout(timer);
-            console.log("input");
-        })
-        .on('error', function(err) {
-            clearTimeout(timer);
-            if (err.code == "ENOTFOUND") {
-                console.log("[ERROR] No device found at this address!");
-                clientSocket.destroy();
-                return;
-            }
+document.getElementById('downloadFromBlockchain').onclick = function() {
+    let fileName = document.getElementById('fileNameToDownload').value;
+    doLog(fileName);
+};
 
-            if (err.code == "ECONNREFUSED") {
-                console.log("[ERROR] Connection refused! Please check the IP.");
-                clientSocket.destroy();
-                return;
-            }
+document.getElementById('getUploadHistory').onclick = function() {
+    uploadHistoryTA = document.getElementById('uploadHistoryTextArea');
+};
 
-
-            console.log("[CONNECTION] Unexpected error! " + err.message + "     RESTARTING SERVER");
-
-
-            process.exit(1);
-
-        })
-        .on('disconnect', function() {
-            console.log("[CONNECTION] disconnected!");
-        });
-    timer = setTimeout(function() {
-        console.log("[ERROR] Attempt at connection exceeded timeout value");
-        clientSocket.end();
-    }, timeout);
-} catch(err) {
-    console.log("[CONNECTION] connection failed! " + err);
+function doLog(message) {
+   console.log(new Date().getTime()+":\t"+message);
 }
-
-// document.getElementById('signIn').onclick = function(){
-//   var seed = document.getElementById('seedInputField').value;
-//
-//   client.invoke("signIn", seed, function(error, res, more) {
-//       if(error) {
-//          console.error(error);
-//       } else {
-//           doLog(res.address);
-//       }
-//   });
-// };
-//
-// document.getElementById('uploadToBlockchain').onclick = function(){
-//   var filePath = document.getElementById('fileToUpload').files[0].path;
-//
-//   client.invoke("uploadFileToBlockchain", filePath, function(error, res, more) {
-//       //doLog(res.fileName+"\t"+res.uploadInfo.length+" pieces");
-//   });
-// };
-//
-// document.getElementById('downloadFromBlockchain').onclick = function() {
-//   var fileName = document.getElementById('fileNameToDownload').value;
-//
-//   client.invoke("downloadAndSaveFileFromBlockchain", fileName, function(error, res, more) {
-//         doLog(res)
-//   });
-// }
-//
-// document.getElementById('getUploadHistory').onclick = function() {
-//   client.invoke("getUploadHistory", function(error, res, more) {
-//     uploadHistoryTA = document.getElementById('uploadHistoryTextArea')
-//     uploadHistoryTA.rows = res.length
-//     uploadHistoryTA.value = res.join('\n')
-//   });
-// }
-//
-// function doLog(message) {
-//    console.log(new Date().getTime()+":\t"+message);
-// }
